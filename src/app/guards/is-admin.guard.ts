1c1b9809d502a29b1fc51167d4b77a611c0b7963
Medali-bejaoui <medali.bejaoui01@gmail.com>
2023-12-28 22:48:07 +0100

    test
    First commit

---
+++ src/app/guards/is-admin.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IsAdminGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (localStorage.getItem('role') == 'responsable') {
+      this.router.navigateByUrl('/ResponsabilityCenter');
+      return false;
+    }else if (localStorage.getItem('role') == 'membreConseil') {
+      this.router.navigateByUrl('/vote');
+      return false;
+    }else if (localStorage.getItem('role').toLowerCase() == 'admin'){
+      return true;
+    }
+    return false;
+  }
+}
